81d95b6131427dd03cce338887ee2269df762334
seixasxbr <mateus.seixas@tidewise.io>
2026-05-27 12:09:08 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,16 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace motors_yanmar_4lv150 {
+    class DummyClass {
+    public:
+        /**
+         * Print a welcome to stdout
+         * \return nothing
+         */
+        void welcome();
+    };
+
+} // end namespace motors_yanmar_4lv150
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
